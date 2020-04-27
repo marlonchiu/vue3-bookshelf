51364e424a6a60c317eb854dc7653f0469b5f8ce
marlonchiu <Knight1898@outlook.com>
2020-04-27 16:46:02 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
+  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-extra-semi": 2 // 禁止多余的分号
   },
   overrides: [
     {
@@ -28,4 +24,4 @@ module.exports = {
       }
     }
   ]
-};
+}
--- README.md
+++ README.md
@@ -1,29 +1,4 @@
 # vue3-bookshelf
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+* [Fork | Vue3的composition-api + TypeScript实现小型的书本管理](https://github.com/sl1673495/vue-bookshelf)
+* [掘金文章](https://juejin.im/post/5e0da5606fb9a048483ecf64)
